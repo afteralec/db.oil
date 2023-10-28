429abb4971d0ad94c3027e4134f673b8125deb94
Alec DuBois <after.alec@gmail.com>
2023-10-27 17:57:33 -0700

    Remove foreign key from player permissions

--- migrations/20231027235907_create_player_permissions.sql
+++ migrations/20231027235907_create_player_permissions.sql
@@ -2,6 +2,6 @@ CREATE TABLE player_permissions (
   id bigint NOT NULL AUTO_INCREMENT PRIMARY KEY,
   pid bigint NOT NULL,
   permission varchar(128) NOT NULL,
-  CONSTRAINT FOREIGN KEY player_permissions_player (pid) REFERENCES players (id),
+  INDEX player_permissions_pid (pid),
   UNIQUE INDEX player_permissions_permission (permission)
 ) CHARSET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
