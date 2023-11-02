81775759e3dabe8dacc2373a6ebca8782163a36f
Alec DuBois <after.alec@gmail.com>
2023-11-01 20:56:30 -0700

    Update indexes on emails

--- migrations/20231027083329_create_emails.sql
+++ migrations/20231027083329_create_emails.sql
@@ -4,5 +4,7 @@ CREATE TABLE emails (
   updated_at datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   verified bool NOT NULL,
   pid bigint NOT NULL,
-  id bigint NOT NULL AUTO_INCREMENT PRIMARY KEY
+  id bigint NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  INDEX emails_player_id (pid),
+  UNIQUE INDEX emails_player_id_address (pid, address)
 ) CHARSET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
