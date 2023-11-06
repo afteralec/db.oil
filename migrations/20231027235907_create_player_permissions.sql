3871a831ed85edf76933476778376ceeb0647b99
Alec DuBois <after.alec@gmail.com>
2023-11-06 12:58:55 -0800

    Update models

--- migrations/20231027235907_create_player_permissions.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE player_permissions (
-  permission varchar(128) NOT NULL,
-  created_at datetime DEFAULT CURRENT_TIMESTAMP,
-  updated_at datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  pid bigint NOT NULL,
-  id bigint NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  INDEX player_permissions_pid (pid),
-  UNIQUE INDEX player_permissions_permission (pid, permission)
-) CHARSET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
