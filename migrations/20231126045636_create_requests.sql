ef33752238e3831e40897017d7c31acdabc8e2c2
Alec DuBois <after.alec@gmail.com>
2023-12-07 15:31:31 -0800

    Update comment models and optimize space on character application model

--- migrations/20231126045636_create_requests.sql
+++ migrations/20231126045636_create_requests.sql
@@ -1,6 +1,7 @@
 CREATE TABLE IF NOT EXISTS requests
 (
   type            varchar(32) NOT NULL,
+  status          varchar(10) NOT NULL DEFAULT "Incomplete",
   created_at      datetime DEFAULT CURRENT_TIMESTAMP,
   updated_at      datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   vid             bigint NOT NULL DEFAULT 0,
@@ -9,4 +10,16 @@ CREATE TABLE IF NOT EXISTS requests
   INDEX requests_type (type),
   INDEX requests_pid (pid),
   INDEX requests_vid (vid)
+) CHARSET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
+
+CREATE TABLE IF NOT EXISTS request_status_changes
+(
+  status  varchar(10) NOT NULL DEFAULT "Incomplete",
+  created_at datetime DEFAULT CURRENT_TIMESTAMP,
+  vid bigint NOT NULL DEFAULT 0,
+  rid bigint NOT NULL,
+  pid bigint NOT NULL,
+  id bigint NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  INDEX request_status_changes_rid (rid),
+  INDEX request_status_changes_pid (pid)
 ) CHARSET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
--- migrations/20231126054040_create_request_comments.sql
+++ migrations/20231126054040_create_request_comments.sql
@@ -3,6 +3,7 @@ CREATE TABLE IF NOT EXISTS request_comments
   created_at          datetime DEFAULT CURRENT_TIMESTAMP,
   updated_at          datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   deleted_at          datetime,
+  text                varchar(998) NOT NULL,
   field               varchar(32),
   deleted             bool DEFAULT FALSE,
   cid                 bigint,
@@ -14,15 +15,6 @@ CREATE TABLE IF NOT EXISTS request_comments
   INDEX request_comments_rid (rid),
   INDEX request_comments_cid (cid),
   INDEX request_comments_vid (vid)
-) CHARSET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
-
-CREATE TABLE IF NOT EXISTS request_comment_content
-(
-  created_at      datetime DEFAULT CURRENT_TIMESTAMP,
-  text            varchar(998) NOT NULL,
-  cid             bigint NOT NULL,
-  id              bigint NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  UNIQUE INDEX request_comment_content_cid (cid)
 ) CHARSET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
 
 CREATE TABLE IF NOT EXISTS request_comment_content_history
