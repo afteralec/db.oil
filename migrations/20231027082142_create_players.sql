e225b3b7d4c8c10e68b7dcdaa5f68b0dcd4dd8e3
Alec DuBois <after.alec@gmail.com>
2023-12-14 22:59:39 -0800

    Minor cleanup

--- migrations/20231027082142_create_players.sql
+++ migrations/20231027082142_create_players.sql
@@ -3,7 +3,6 @@ CREATE TABLE players (
   updated_at      datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   pw_hash         varchar(255) NOT NULL,
   username        varchar(16) NOT NULL,
-  role            varchar(16) NOT NULL,
   id              bigint NOT NULL AUTO_INCREMENT PRIMARY KEY,
   UNIQUE INDEX players_username (username)
 ) CHARSET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
--- migrations/20231213092259_create_player_permissions.sql
+++ migrations/20231213092259_create_player_permissions.sql
@@ -1,20 +1,23 @@
 CREATE TABLE IF NOT EXISTS player_permissions 
 (
   created_at    datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  updated_at    datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  permissions   varchar(101) NOT NULL,
+  permission    varchar(101) NOT NULL,
   ipid          bigint NOT NULL,
   pid           bigint NOT NULL,
-  id            bigint NOT NULL AUTO_INCREMENT PRIMARY KEY
+  id            bigint NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  UNIQUE INDEX player_permissions_permission (pid, permission),
+  INDEX player_permissions_pid (pid),
+  INDEX player_permissions_ipid (ipid)
 ) CHARSET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
 
 CREATE TABLE IF NOT EXISTS player_permission_change_history
 (
   created_at    datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  updated_at    datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  permissions   varchar(101) NOT NULL,
+  permission    varchar(101) NOT NULL,
   ipid          bigint NOT NULL,
   pid           bigint NOT NULL,
   id            bigint NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  deleted       bool NOT NULL DEFAULT FALSE
+  revoked       bool NOT NULL DEFAULT FALSE,
+  INDEX player_permission_change_history_pid (pid),
+  INDEX player_permission_change_history_ipid (ipid)
 ) CHARSET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
