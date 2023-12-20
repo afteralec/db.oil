2197984339b0d24da0024d8643cd321fd744e8ff
Alec DuBois <after.alec@gmail.com>
2023-12-20 00:22:05 -0800

    Minor cleanup

--- migrations/20231126045636_create_requests.sql
+++ migrations/20231126045636_create_requests.sql
@@ -8,13 +8,12 @@ CREATE TABLE IF NOT EXISTS requests
   pid             bigint NOT NULL,
   id              bigint NOT NULL AUTO_INCREMENT PRIMARY KEY,
   vid             smallint NOT NULL DEFAULT 0,
-  new             bool NOT NULL DEFAULT false,
   INDEX requests_type (type),
   INDEX requests_pid (pid),
   INDEX requests_vid (vid)
 ) CHARSET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
 
-CREATE TABLE IF NOT EXISTS request_status_changes
+CREATE TABLE IF NOT EXISTS request_status_change_history
 (
   created_at  datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   status      varchar(10) NOT NULL DEFAULT "Incomplete",
