36fdf85a6c98f61409b00e34c330b909a0688f74
Alec DuBois <after.alec@gmail.com>
2024-01-09 12:33:01 -0800

    Add help file category and tags

--- migrations/20240109044138_create_help.sql
+++ migrations/20240109044138_create_help.sql
@@ -6,16 +6,25 @@ CREATE TABLE IF NOT EXISTS help
   raw           text NOT NULL,
   sub           varchar(500) NOT NULL,
   title         varchar(250) NOT NULL,
+  category      varchar(150) NOT NULL,
   slug          varchar(50) NOT NULL PRIMARY KEY,
   pid           bigint NOT NULL,
   INDEX help_pid(pid)
 ) CHARSET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
 
+CREATE TABLE IF NOT EXISTS help_tags
+(
+  tag     varchar(50) NOT NULL,
+  slug    varchar(50) NOT NULL,
+  id      bigint NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  INDEX help_tags_slug(slug)
+) CHARSET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
+
 CREATE TABLE IF NOT EXISTS help_related
 (
-  related_sub     varchar(500) NOT NULL,
-  related_title   varchar(250) NOT NULL,
-  related         varchar(50) NOT NULL,
-  slug            varchar(50) NOT NULL PRIMARY KEY,
-  INDEX help_related(related)
+  related_sub       varchar(500) NOT NULL,
+  related_title     varchar(250) NOT NULL,
+  related_slug      varchar(50) NOT NULL,
+  slug              varchar(50) NOT NULL PRIMARY KEY,
+  INDEX help_related_slug(related_slug)
 ) CHARSET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
