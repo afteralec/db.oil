05fca2df232245bbd4c950ffb11250030529bb32
Alec DuBois <after.alec@gmail.com>
2024-01-18 01:08:34 -0800

    Fix IDs

--- migrations/20240116060511_create_rooms.sql
+++ migrations/20240116060511_create_rooms.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS room_images
   description   varchar(2000) NOT NULL,
   title         varchar(150) NOT NULL,
   name          varchar(50) NOT NULL,
-  id            bigint NOT NULL PRIMARY KEY,
+  id            bigint NOT NULL AUTO_INCREMENT PRIMARY KEY,
   size          tinyint NOT NULL DEFAULT 1,
   INDEX room_images_title (title),
   INDEX room_images_size (size),
@@ -26,6 +26,6 @@ CREATE TABLE IF NOT EXISTS rooms
   west          bigint NOT NULL DEFAULT 0,
   northwest     bigint NOT NULL DEFAULT 0,
   riid          bigint NOT NULL,
-  id            bigint NOT NULL PRIMARY KEY,
+  id            bigint NOT NULL AUTO_INCREMENT PRIMARY KEY,
   UNIQUE INDEX rooms_riid (riid)
 ) CHARSET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
