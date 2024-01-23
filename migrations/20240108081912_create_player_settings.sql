c8b148df71ab6517cf2019e5e09e665da847818d
Alec DuBois <after.alec@gmail.com>
2024-01-22 21:07:38 -0800

    Remove room images and fix default theme

--- migrations/20240108081912_create_player_settings.sql
+++ migrations/20240108081912_create_player_settings.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS player_settings
 (
   created_at  datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   updated_at  datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  theme       varchar(20) NOT NULL DEFAULT "Light",
+  theme       varchar(20) NOT NULL,
   pid         bigint NOT NULL,
   id          bigint NOT NULL AUTO_INCREMENT PRIMARY KEY,
   UNIQUE INDEX player_settings_pid (pid)
--- migrations/20240116060511_create_rooms.sql
+++ migrations/20240116060511_create_rooms.sql
@@ -1,22 +1,10 @@
-CREATE TABLE IF NOT EXISTS room_images
-(
-  created_at    datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  updated_at    datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  description   varchar(2000) NOT NULL,
-  title         varchar(150) NOT NULL,
-  name          varchar(50) NOT NULL,
-  id            bigint NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  size          tinyint NOT NULL DEFAULT 1,
-  INDEX room_images_title (title),
-  INDEX room_images_size (size),
-  UNIQUE INDEX room_images_name (name)
-) CHARSET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
-
 -- TODO: Index the exits to support search and validation
 CREATE TABLE IF NOT EXISTS rooms
 (
   created_at    datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   updated_at    datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  description   varchar(2000) NOT NULL,
+  title         varchar(150) NOT NULL,
   north         bigint NOT NULL DEFAULT 0,
   northeast     bigint NOT NULL DEFAULT 0,
   east          bigint NOT NULL DEFAULT 0,
@@ -25,7 +13,8 @@ CREATE TABLE IF NOT EXISTS rooms
   southwest     bigint NOT NULL DEFAULT 0,
   west          bigint NOT NULL DEFAULT 0,
   northwest     bigint NOT NULL DEFAULT 0,
-  riid          bigint NOT NULL,
   id            bigint NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  UNIQUE INDEX rooms_riid (riid)
+  size          tinyint NOT NULL DEFAULT 1,
+  unmodified    bool NOT NULL DEFAULT true,
+  INDEX rooms_title (title)
 ) CHARSET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
