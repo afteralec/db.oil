230692715656574243dc598180b191713da5d442
Alec DuBois <after.alec@gmail.com>
2024-01-28 22:48:05 -0800

    Update actor image data modeling

--- migrations/20240116063339_create_actors.sql
+++ migrations/20240116063339_create_actors.sql
@@ -1,37 +1,14 @@
 CREATE TABLE IF NOT EXISTS actor_images
 (
-  created_at    datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  updated_at    datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  description             varchar(2000) NOT NULL,
-  short_description       varchar(300) NOT NULL,
-  name                    varchar(50) NOT NULL,
-  gender                  varchar(32) NOT NULL,
-  eats_into               bigint NOT NULL DEFAULT 0,
-  sustenance              smallint NOT NULL DEFAULT 0,
-  id                      bigint NOT NULL PRIMARY KEY,
-  liquid_capacity         smallint NOT NULL DEFAULT 0,
-  seating                 tinyint NOT NULL DEFAULT 0,
-  is_container            boolean NOT NULL DEFAULT false,
-  is_surface_container    boolean NOT NULL DEFAULT false,
-  uniq                    boolean NOT NULL DEFAULT false
-) CHARSET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
-
-CREATE TABLE IF NOT EXISTS actor_images_can
-(
-  created_at    datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  updated_at    datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  can     varchar(50) NOT NULL,
-  id      bigint NOT NULL PRIMARY KEY,
-  INDEX actor_images_can (can)
-) CHARSET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
-
-CREATE TABLE IF NOT EXISTS actor_images_can_be
-(
-  created_at    datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  updated_at    datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  can_be      varchar(50) NOT NULL,
-  id          bigint NOT NULL PRIMARY KEY,
-  INDEX actor_images_can_be (can_be)
+  created_at            datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at            datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  description           varchar(2000) NOT NULL,
+  short_description     varchar(300) NOT NULL,
+  name                  varchar(50) NOT NULL,
+  gender                varchar(32) NOT NULL,
+  id                    bigint NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  uniq                  boolean NOT NULL DEFAULT false,
+  UNIQUE INDEX actor_images_name (name)
 ) CHARSET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
 
 CREATE TABLE IF NOT EXISTS actor_images_keywords
@@ -40,9 +17,34 @@ CREATE TABLE IF NOT EXISTS actor_images_keywords
   updated_at    datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   keyword       varchar(10) NOT NULL,
   aiid          bigint NOT NULL,
-  id            bigint NOT NULL PRIMARY KEY,
+  id            bigint NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  INDEX actor_images_keywords_aiid (aiid),
   INDEX actor_images_keywords_keyword (keyword),
-  UNIQUE INDEX actor_images_keywords_aiid (aiid)
+  UNIQUE INDEX actor_images_keywords_aiid_keyword (aiid, keyword)
+) CHARSET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
+
+CREATE TABLE IF NOT EXISTS actor_images_can
+(
+  created_at    datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at    datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  can           varchar(50) NOT NULL,
+  aiid          bigint NOT NULL,
+  id            bigint NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  INDEX actor_images_can_aiid (aiid),
+  INDEX actor_images_can_can (can),
+  UNIQUE INDEX actor_images_can_aiid_can (aiid, can)
+) CHARSET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
+
+CREATE TABLE IF NOT EXISTS actor_images_can_be
+(
+  created_at    datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at    datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  can_be        varchar(50) NOT NULL,
+  aiid          bigint NOT NULL,
+  id            bigint NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  INDEX actor_images_can_be_aiid (aiid),
+  INDEX actor_images_can_be_can_be (can_be),
+  UNIQUE INDEX actor_images_can_be_aiid_can_be (aiid, can_be)
 ) CHARSET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
 
 CREATE TABLE IF NOT EXISTS actor_images_hands
@@ -50,20 +52,54 @@ CREATE TABLE IF NOT EXISTS actor_images_hands
   created_at    datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   updated_at    datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   aiid          bigint NOT NULL,
-  id            bigint NOT NULL PRIMARY KEY,
+  id            bigint NOT NULL AUTO_INCREMENT PRIMARY KEY,
   hand          tinyint NOT NULL DEFAULT 1,
-  INDEX actor_images_hands_hand (hand),
   INDEX actor_images_hands_aiid (aiid),
+  INDEX actor_images_hands_hand (hand),
   UNIQUE INDEX actor_images_hands_aiid_hand (aiid, hand)
 ) CHARSET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
 
-CREATE TABLE IF NOT EXISTS actor_images_primary_hand
+CREATE TABLE IF NOT EXISTS actor_images_primary_hands
 (
   created_at    datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   updated_at    datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   aiid          bigint NOT NULL,
-  id            bigint NOT NULL PRIMARY KEY,
+  id            bigint NOT NULL AUTO_INCREMENT PRIMARY KEY,
   hand          tinyint NOT NULL DEFAULT 1,
   INDEX actor_images_primary_hand_aiid (aiid),
+  INDEX actor_images_primary_hand_hand (hand),
   UNIQUE INDEX actor_images_primary_hand_aiid_hand (aiid, hand)
+) CHARSET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
+
+CREATE TABLE IF NOT EXISTS actor_images_container_properties
+(
+  created_at              datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at              datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  liquid_capacity         smallint NOT NULL DEFAULT 0,
+  aiid                    bigint NOT NULL,
+  id                      bigint NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  is_container            boolean NOT NULL DEFAULT false,
+  is_surface_container    boolean NOT NULL DEFAULT false,
+  UNIQUE INDEX actor_images_food_properties_aiid (aiid)
+) CHARSET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
+
+CREATE TABLE IF NOT EXISTS actor_images_food_properties
+(
+  created_at    datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at    datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  eats_into     bigint NOT NULL DEFAULT 0,
+  aiid          bigint NOT NULL,
+  id            bigint NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  sustenance    smallint NOT NULL DEFAULT 0,
+  UNIQUE INDEX actor_images_food_properties_aiid (aiid)
+) CHARSET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
+
+CREATE TABLE IF NOT EXISTS actor_images_furniture_properties
+(
+  created_at      datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at      datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  aiid            bigint NOT NULL,
+  id              bigint NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  seating         tinyint NOT NULL DEFAULT 0,
+  UNIQUE INDEX actor_images_food_properties_aiid (aiid)
 ) CHARSET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
