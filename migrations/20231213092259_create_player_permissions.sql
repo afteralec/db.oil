8f383f5f937fdb27f664628f70a2abfb6093c688
Alec DuBois <after.alec@gmail.com>
2024-02-18 20:39:53 -0800

    Update permissions modeling

--- migrations/20231213092259_create_player_permissions.sql
+++ migrations/20231213092259_create_player_permissions.sql
@@ -1,11 +1,11 @@
 CREATE TABLE IF NOT EXISTS player_permissions 
 (
   created_at  datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  permission  varchar(101) NOT NULL,
+  name        varchar(101) NOT NULL,
   ipid        bigint NOT NULL,
   pid         bigint NOT NULL,
   id          bigint NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  UNIQUE INDEX player_permissions_permission (pid, permission),
+  UNIQUE INDEX player_permissions_name (pid, name),
   INDEX player_permissions_pid (pid),
   INDEX player_permissions_ipid (ipid)
 ) CHARSET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
@@ -13,7 +13,7 @@ CREATE TABLE IF NOT EXISTS player_permissions
 CREATE TABLE IF NOT EXISTS player_permission_change_history
 (
   created_at  datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  permission  varchar(101) NOT NULL,
+  name        varchar(101) NOT NULL,
   ipid        bigint NOT NULL,
   pid         bigint NOT NULL,
   id          bigint NOT NULL AUTO_INCREMENT PRIMARY KEY,
