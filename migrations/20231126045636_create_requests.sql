dd3eb8167d83e1d4d59b955ff73a00460502ee2f
Alec DuBois <after.alec@gmail.com>
2024-04-29 14:51:35 -0700

    Remove Defunct Actions
    All of the previous CI actions relied on Planetscale as the DB; this
    removes that and includes some minor updates for the new request data
    structure.

--- migrations/20231126045636_create_requests.sql
+++ migrations/20231126045636_create_requests.sql
@@ -2,41 +2,65 @@ CREATE TABLE IF NOT EXISTS requests
 (
   created_at      datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   updated_at      datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  type            varchar(32) NOT NULL,
-  status          varchar(10) NOT NULL DEFAULT "Incomplete",
+  type            varchar(50) NOT NULL,
+  status          varchar(10) NOT NULL,
   rpid            bigint NOT NULL DEFAULT 0,
   pid             bigint NOT NULL,
   id              bigint NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  vid             smallint NOT NULL DEFAULT 0,
   INDEX requests_type (type),
   INDEX requests_pid (pid),
-  INDEX requests_vid (vid)
+  INDEX requests_rpid (rpid)
+) CHARSET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
+
+CREATE TABLE IF NOT EXISTS request_fields
+(
+  created_at      datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at      datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  value           varchar(10000) NOT NULL,
+  type            varchar(50) NOT NULL,
+  status          varchar(11) NOT NULL,
+  rid             bigint NOT NULL,
+  id              bigint NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  INDEX request_fields_rid (rid),
+  INDEX request_fields_type (type),
+  UNIQUE INDEX request_fields_rid_type (rid, type)
+) CHARSET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
+
+CREATE TABLE IF NOT EXISTS open_request_change_requests
+(
+  created_at    datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at    datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  value         varchar(10000) NOT NULL,
+  text          varchar(1000) NOT NULL,
+  rfid          bigint NOT NULL,
+  pid           bigint NOT NULL,
+  id            bigint NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  UNIQUE INDEX open_request_change_requests_rfid (rfid),
+  INDEX open_request_change_requests_pid (pid)
 ) CHARSET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
 
 CREATE TABLE IF NOT EXISTS request_change_requests
 (
   created_at    datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   updated_at    datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  value         varchar(10000) NOT NULL,
   text          varchar(1000) NOT NULL,
-  field         varchar(50) NOT NULL DEFAULT "",
-  rid           bigint NOT NULL,
+  rfid          bigint NOT NULL,
   pid           bigint NOT NULL,
   id            bigint NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  locked        boolean NOT NULL DEFAULT false,
-  old           boolean NOT NULL DEFAULT false,
-  INDEX request_change_requests_rid (rid),
-  INDEX request_change_requests_pid (pid),
-  INDEX request_change_requests_rid_field (rid, field)
+  UNIQUE INDEX request_change_requests_rfid (rfid),
+  INDEX request_change_requests_pid (pid)
 ) CHARSET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
 
-CREATE TABLE IF NOT EXISTS request_status_change_history
+CREATE TABLE IF NOT EXISTS past_request_change_requests
 (
-  created_at  datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  status      varchar(10) NOT NULL DEFAULT "Incomplete",
-  rid         bigint NOT NULL,
-  pid         bigint NOT NULL,
-  id          bigint NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  vid         smallint NOT NULL DEFAULT 0,
-  INDEX request_status_changes_rid (rid),
-  INDEX request_status_changes_pid (pid)
+  created_at    datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at    datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  value         varchar(10000) NOT NULL,
+  text          varchar(1000) NOT NULL,
+  rfid          bigint NOT NULL,
+  pid           bigint NOT NULL,
+  id            bigint NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  INDEX past_request_change_requests_rfid (rfid),
+  INDEX past_request_change_requests_pid (pid)
 ) CHARSET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
